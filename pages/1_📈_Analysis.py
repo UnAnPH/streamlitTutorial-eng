d9724f26d824e413c41e6406622d7b211a22142d
Jayson Andal <jaysonandal50@gmail.com>
2023-05-29 23:48:14 +0200

    #1

--- pages/1_📈_Analysis.py
+++ pages/1_📈_Analysis.py
@@ -4,8 +4,37 @@ import pandas as pd
 
 # each tab has a separate function
 
+
+def create_products_tab(products_tab):
+    col1, col2, col3 = products_tab.columns(3)
+    
+    payment_info = execute_query(st.session_state["connection"],
+                                 "SELECT SUM(amount) AS 'Total Amount', MAX(amount) AS 'Max Payment', AVG(amount) AS 'Average Payment' FROM payments")
+    
+    payment_infos = [dict(zip(payment_info.keys(), result)) for result in payment_info]
+    
+    col1.metric("Total Amount", compact_num(payment_infos[0]["Total Amount"]))
+    col2.metric("Max Payment", compact_num(payment_infos[0]["Max Payment"]))
+    col3.metric("Average Payment", compact_num(payment_infos[0]["Average Payment"]))
+    
+    with products_tab.expander("Product Overview",True):
+        prod_col1, prod_col2, prod_col3 = products_tab.columns(3)
+        sort_param = prod_col1.radio("Sort by", ["code", "name", "quantity", "price"])
+        sort_choice = prod_col2.selectbox("Order", ["ASC", "DESC"])
+        
+        if prod_col1.button("Show", type = "primary"):
+            query = f"SELECT productCode AS 'code', productName AS 'name', quantityInStock AS quantity, buyPrice AS price, MSRP FROM products ORDER BY {sort_param} {sort_choice}"
+            products = execute_query(st.session_state["connection"], query)
+            df_products = pd.DataFrame(products, columns = products.keys())
+            st.dataframe(df_products)
+    return
+
 if __name__ == "__main__":
     st.title("📈 Analysis")
 
     # creation of separate tabs
     products_tab, staff_tab, customers_tab = st.tabs(["Products", "Staff", "Customers"])
+
+    if check_connection():
+        create_products_tab(products_tab)
+ 
--- utils/utils.py
+++ utils/utils.py
@@ -2,3 +2,42 @@ import streamlit as st
 from sqlalchemy import create_engine,text
 
 """Collects the main functions shared by the various pages"""
+
+def connect_db(dialect,username,password,host,dbname):
+    try:
+        engine = create_engine(f"{dialect}://{username}:{password}@{host}/{dbname}")
+        conn = engine.connect()
+        return conn
+    except:
+        st.error("Unable to connect to database")
+        return False
+        
+    
+def execute_query(conn,query):
+    return conn.execute(text(query))
+
+def check_connection():
+    if "connection" not in st.session_state.keys():
+        st.session_state["connection"]= False
+    
+    if st.sidebar.button("Connect to the database"):
+        myconnection = connect_db("mysql+pymysql","root","mypassword","localhost","classicmodels")
+        if myconnection is not False:
+            st.session_state["connection"]= myconnection
+        else:
+            st.session_state["connection"]= False
+            st.sidebar.error("Unable to connect to database")
+            
+    if st.session_state["connection"]:
+        st.sidebar.success("Connected to database")
+        return True
+    
+def compact_num(number):
+    if number < 1000:
+        return number
+    elif number < 1000000:
+        return str(round(number/1000,1))+"K"
+    elif number < 1000000000:
+        return str(round(number/1000000,1))+"M"
+    else:
+        return str(round(number/1000000000,1))+"B"
